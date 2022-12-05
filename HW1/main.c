f40274014d57ebace42371df8fcdc359d2a71504
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2022-12-06 01:16:45 +0200

    Update main.c
    commit with getCentroids

--- HW1/main.c
+++ HW1/main.c
@@ -4,6 +4,12 @@
 
 float e = 0.001;
 
+//decleration of the funcs
+int inputValidation(int k, int iter, int num_of_dp);
+struct vector *getCentroids(struct vector *dp_head, int k);
+struct cord* createVector(struct cord *x);
+
+//decleration of the structs
 struct cord
 {
     double value;
@@ -14,41 +20,46 @@ struct vector
     struct vector *next;
     struct cord *cords;
 };
-
-// we need to build a new struct for clusters
-
+struct cluster
+{
+    struct cluster *next_cluster;
+    struct vector *next_vector;
+};
 
 int main(int argc, char **argv)
 {
-
-    printf("2");
-    // decleretion of structs: headVector & cord
-    struct vector *head_vec, *curr_vec, *next_vec;
+    struct vector *dp_head, *curr_vec, *next_vec;
     struct cord *head_cord, *curr_cord, *next_cord;
     int i, j, rows = 0, cols;
     double n;
     char c;
 
-    // init of head cord
+    int k;
+    int iter;
+    int num_of_dp;
+
+    k = atoi(argv[1]);
+    iter = atoi(argv[2]);
+
+
+
+
     head_cord = malloc(sizeof(struct cord));
     curr_cord = head_cord;
     curr_cord->next = NULL;
 
-    // init of head_vec
-    head_vec = malloc(sizeof(struct vector));
-    curr_vec = head_vec;
+    dp_head = malloc(sizeof(struct vector));
+    curr_vec = dp_head;
     curr_vec->next = NULL;
 
+    num_of_dp = 0;
 
     while (scanf("%lf%c", &n, &c) == 2)
-
     {
-        printf("error 1");
-
         if (c == '\n')
         {
-
-            curr_cord->value = n; // means: vec[i][j] = n
+            num_of_dp++;
+            curr_cord->value = n;
             curr_vec->cords = head_cord;
             curr_vec->next = malloc(sizeof(struct vector));
             curr_vec = curr_vec->next;
@@ -58,129 +69,38 @@ int main(int argc, char **argv)
             curr_cord->next = NULL;
             continue;
         }
-
         curr_cord->value = n;
         curr_cord->next = malloc(sizeof(struct cord));
         curr_cord = curr_cord->next;
         curr_cord->next = NULL;
+
     }
 
+    if (inputValidation(k, iter, num_of_dp)){
+        return 0;
+    }
 
-    printf("1");
-    //this is where we should put out code at just like in python
-    //
-    //
-    //
-    //
-
-    //this is where we should delete all out vetcots
-    //
-    //
-    //
-    //
+    struct vector *centroids_head_vec;
+    centroids_head_vec = malloc(sizeof(struct vector));
+    centroids_head_vec = getCentroids(dp_head, k); //creates the k centroids
 
     return 0;
 }
 
-*
-void kmeans(int k, int iter = 200,struct vector *dataPoints) {
-    float e = 0.001;
-    int convergenceCNT = 0;
-    int iterCNT = 0;
 
-    struct vector centroids;
-    int[] converganceArray = calloc(k,sizeof(int));
-
-
-
-    // init new cluster vector size k & new array of clusters-size, init as all zeros
-
-    struct vector curr = *dataPoints; // this is the head_vec
-    while((iterCNT<=iter) || (convergenceCNT<=k) {
-
-        int[] cluster_size_array = calloc(k,sizeof(int));
-        // init of new cluster array of vectors
-
-        while (curr->next != NULL) { // iterates over x in datapoints
-
-            int cnt = 0;
-            int min_index;
-            float min_val = INFINITY;
-            struct cord curr_centroid = head_centroids;
-
-            while(curr_centroid->next != NULL){ //this iterates over the centroids
-
-                float d = euclideanDistance(curr, curr_centroid);
-                if(min_val < d){
-                    min_index = cnt;
-                    min_val = d;
-                }
-                cnt++
-
-
-            }
-            struct vector tmp = cluster[cnt];
-
-
-            }
-            struct vector tmp = curr_cluster->next;
-            curr_cluster->next =
-        }
+int inputValidation(int k, int iter, int num_of_dp){
+    if((k<=1) || (k>=num_of_dp) || (k%1 !=0)){
+        printf("Invalid number of clusters!");
+        return 1;
     }
-
-
+    if((iter <= 1) || (iter >= 1000) || (iter % 1 != 0)) {
+        printf("Invalid maximum iteration!");
+        return 1;
+    }
+    return 0;
 }
 
-
-vector,int updateCentroid(vector clusters, vector centroids, int convergenceCNT){
-
-}
-
-struct vector getCentroids(struct vector *dataPoints, int k) {
-
-
-
-    struct vector *centroids_head_vec, *centroids_curr_vec, *centroids_next_vec;
-    struct cord *centroids_head_cord, *centroids_curr_cord, *centroids_next_cord;
-
-    centroids_head_cord = malloc(sizeof(struct cord));
-    centroids_curr_cord = centroids_head_cord;
-    centroids_curr_cord->next = NULL;
-
-    // init of head_vec
-    centroids_head_vec = malloc(sizeof(struct vector));
-    centroids_curr_vec = centroids_head_vec;
-    centroids_curr_vec->next = NULL;
-
-
-
-    int i;
-    for( i=0; i<k; i++){
-        float val = curr_cord->value;
-        while(curr_cord->next != NULL){
-            curr_cord = curr_cord->next;
-            centroids_curr_cord->value = val;
-            centroids_curr_cord->next = malloc(sizeof(struct cord));
-            centroids_curr_cord = centroids_curr_cord->next;
-            centroids_curr_cord->next = NULL;
-
-        }
-        centroids_curr_cord->value = val; // means: vec[i][j] = n
-        centroids_curr_vec->cords = centroids_head_cord;
-        centroids_curr_vec->next = malloc(sizeof(struct vector));
-        centroids_curr_vec = centroids_curr_vec->next;
-        centroids_curr_vec->next = NULL;
-        centroids_head_cord = malloc(sizeof(struct cord));
-        centroids_curr_cord = centroids_head_cord;
-        centroids_curr_cord->next = NULL;
-    };
-}
-
-
-
-
-
-float euclideanDistance(struct cord *x1,struct cord *x2, int n) {
+float euclideanDistance(struct cord *x1, struct cord *x2){
     float d = 0;
     while (x1->next != NULL){
         d = d + (x1->value - x2->value)*(x1->value - x2->value);
@@ -191,24 +111,48 @@ float euclideanDistance(struct cord *x1,struct cord *x2, int n) {
     return d;
 }
 
-int inputValidation(int k, int iter, int n){ // n = number of dataPoints. we need to check it on main func
-    if((k<=1) || (k>=n) || (k%1 != 0 )){
-        printf("Invalid number of clusters!");
-        return 1;
-    }
-    if((iter<=1) || (iter >= 1000) || (iter%1 != 0)) {
-        printf("Invalid maximum iteration!");
-        return 1;
-    }
-    return 0;
+struct vector *getCentroids(struct vector *dp_head, int k){
+
+    //decleration & init of centroids struct
+    struct vector *centroids_head_vec, *centroids_curr_vec, *centroids_next_vec;
+    struct cord *centroids_head_cord, *centroids_curr_cord, *centroids_next_cord;
+
+    centroids_head_cord = malloc(sizeof(struct cord));
+    centroids_curr_cord = centroids_head_cord;
+    centroids_curr_cord->next = NULL;
+
+    centroids_head_vec = malloc(sizeof(struct vector));
+    centroids_curr_vec = centroids_head_vec;
+    centroids_curr_vec->next = NULL;
+
+    struct vector *curr_vec = dp_head;
+    struct cord *curr_cord = curr_vec->cords;
+
+
+
+    int i;
+    for(i=0; i<k; i++){
+        centroids_curr_vec->cords = malloc(sizeof(struct cord));
+        centroids_curr_vec->cords = createVector(curr_vec->cords);
+        centroids_curr_vec->next = malloc(sizeof(struct vector));
+        centroids_curr_vec = centroids_curr_vec->next;
+        curr_vec=curr_vec->next;
+    };
+    return centroids_head_vec;
 }
 
-void insertFirst(struct vector head, struct cord x){
-    struct vector tmp = head->next;
-    head->next = malloc(sizeof(struct vector));
+//creates a new vector.cords made of the coordinates of a given vector.cords
+struct cord* createVector(struct cord *x){
+    struct cord *res;
+    res = malloc(sizeof(struct cord));
+    struct cord *curr;
+    curr = res;
     while(x->next != NULL){
-
+        curr->value = x->value;
+        curr->next = malloc(sizeof(struct cord));
+        curr = curr->next;
+        x = x->next;
     }
-
-
+    curr->value = x->value;
+    return res;
 }
