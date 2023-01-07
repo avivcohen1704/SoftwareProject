59d734d42ecb29ee0fca2e1e4e30105a99bae3a1
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-01-07 16:31:29 +0200

    not final bonus
    works only on pycharm

---
+++ HW2/bonus.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+dp = load_iris().data
+X = []
+Y = [1,2,3,4,5,6,7,8,9,10]
+centroids = []
+
+for i in range(10):
+    kmeans  = KMeans(n_clusters=i+1, init="k-means++", random_state=0)
+    kmeans.fit(dp)
+    X.append(kmeans.inertia_)
+
+plt.plot(Y,X)
+
+plt.scatter(x=3,y=79,marker="o",color="red")
+plt.scatter(x=2,y=153,marker="o",color="red")
+plt.title('Elbow Method for selection of optimal "k" clusters')
+plt.ylabel("---> inertia")
+plt.xlabel("---> k")
+plt.show()
