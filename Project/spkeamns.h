10d148c540d253f334a5246bce5c3bb239940b3d
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-03-04 15:12:43 +0200

    Update spkeamns.h

--- Project/spkeamns.h
+++ Project/spkeamns.h
@@ -1,1 +1,24 @@
+# ifndef SPKMEANS_H_
+# define SPKMEANS_H_
 
+struct cord
+{
+    double value;
+    struct cord *next;
+};
+struct vector
+{
+    struct vector *next;
+    struct cord *cords;
+};
+
+int ** wam_c(int n, int[n][n] arr);
+
+int ** ddg_c(int n, double[n][n]W);
+
+double ** gl_c(int n, double[n][n] D, double[n][n]W);
+
+double ** jacobi_c(int n, double[n][n] A);
+
+
+# endif
