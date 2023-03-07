76601cc54a3dc5019b8c15afb7f6e6f124a39a3c
omri0702 <100294909+omri0702@users.noreply.github.com>
2023-03-07 21:01:56 +0200

    Update gl.c

--- Project/gl.c
+++ Project/gl.c
@@ -1,20 +1,17 @@
 #include <Python.h>
-#include "spkeamns.h"
-
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
 # include <string.h>
+#include "spkmeans.h"
 
-double ** gl_c(int n, double[n][n] D, double[n][n]W);
 
-double ** gl_c(int n, double[n][n] D, double[n][n]W){
+
+void gl_c(int n, double D[n][n], double W[n][n], double L[n][n]) {
     int i,j;
-    double [n][n] L;
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
             L[i][j]= D[i][j] - W[i][j];
         }
     }
-    return L;
 }
