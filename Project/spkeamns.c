2323021c64d5c2aa4f8c6f9972194cc40ec1a605
omri0702 <100294909+omri0702@users.noreply.github.com>
2023-03-07 20:56:38 +0200

    Update spkeamns.c

--- Project/spkeamns.c
+++ Project/spkeamns.c
@@ -1,20 +1,15 @@
 #include <Python.h>
-#include <spkeamns.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <math.h>
-# include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "spkmeans.h"
 
-struct cord
-{
-    double value;
-    struct cord *next;
-};
-struct vector
-{
-    struct vector *next;
-    struct cord *cords;
-};
+
+void ll_to_arr(int num_of_dp, int size_of_vec, struct vector *dp_head, double res[num_of_dp][size_of_vec]); 
+void output_print(int n ,double matrix[n][n]);
+void output_print_j(int n ,double matrix[n+1][n]);
+int input_valid(int argc, char **argv);
 
 int main(int argc, char **argv){
     if(input_valid(argc,argv)){
@@ -24,6 +19,8 @@ int main(int argc, char **argv){
     struct vector *dp_head, *curr_vec;
     struct cord *head_cord, *curr_cord;
     int num_of_dp, size_of_vec;
+    double n;
+    char c;
 
     size_of_vec = 0;
     num_of_dp = 0;
@@ -66,33 +63,42 @@ int main(int argc, char **argv){
 
 
     }
-    double [num_of_dp][size_of_vec] input_array;
-    
+    size_of_vec++;
+    double input_array [num_of_dp][size_of_vec];
+
     free(head_cord);
-    input_array = ll_to_arr(num_of_dp,size_of_vec,dp_head);
+    
+    ll_to_arr(num_of_dp,size_of_vec,dp_head,input_array);
 
-    if (argv[1] == "wam"){
-        double [num_of_dp][num_of_dp] output_array;
-        output_array = wam(num_of_dp,input_array);
+
+    if (strcmp(argv[1], "wam") == 0){
+        double output_array[num_of_dp][num_of_dp];
+        wam_c(num_of_dp, size_of_vec, input_array, output_array);
         output_print(num_of_dp, output_array);
 
     }
 
-    if (argv[1] == "ddg"){
-        double [num_of_dp][num_of_dp] output_array;
-        output_array = ddg(num_of_dp,input_array);
+    if (strcmp(argv[1], "ddg") == 0){
+        double wam_output[num_of_dp][num_of_dp];
+        double output_array[num_of_dp][num_of_dp];
+        wam_c(num_of_dp,size_of_vec,input_array, wam_output);
+        ddg_c(num_of_dp, wam_output, output_array);
         output_print(num_of_dp, output_array);
     }
     
-    if (argv[1] == "gl"){
-        double [num_of_dp][num_of_dp] output_array;
-        output_array = gl(num_of_dp,input_array);
+    if (strcmp(argv[1], "gl") == 0){
+        double wam_output[num_of_dp][num_of_dp];
+        double ddg_output[num_of_dp][num_of_dp];
+        double output_array[num_of_dp][num_of_dp];
+        wam_c(num_of_dp, size_of_vec,input_array, wam_output);
+        ddg_c(num_of_dp, wam_output, ddg_output);
+        gl_c(num_of_dp, ddg_output,wam_output,output_array);
         output_print(num_of_dp, output_array);
     }
 
-    if (argv[1] == "jacobi"){
-        double [num_of_dp+1][num_of_dp] output_array;
-        output_array = jacobi(num_of_dp,input_array);
+    if (strcmp(argv[1], "jacobi") == 0){
+        double output_array[num_of_dp+1][num_of_dp];
+        jacobi_c(num_of_dp, input_array, output_array);
         output_print_j(num_of_dp, output_array);
 
     }
@@ -100,7 +106,7 @@ int main(int argc, char **argv){
 
 }
 
-void output_print(n ,double[n][n] matrix){
+void output_print(int n ,double matrix[n][n]){
     int i,j;
     for (i=0;i<n;i++){
         for (j=0;j<n;j++){
@@ -112,11 +118,12 @@ void output_print(n ,double[n][n] matrix){
 }
 
 
-void output_print_j(n ,double[n+1][n] matrix){
+void output_print_j(int n ,double matrix[n+1][n]){
     /* we might change this to matrix = matrix.transpose */
     int i,j;
     for (j=0;j<n;j++){
         printf("%.4f" , matrix[n+1][j]);
+        printf(",");
     }
 
     printf("\n");
@@ -132,40 +139,38 @@ void output_print_j(n ,double[n+1][n] matrix){
 
 
 int input_valid(int argc, char **argv){
-    char[4] funcs;
     int i,s;
+    char funcs[4][10] = {"wam","ddg" , "gl" , "jacobi"};
     s =0;
     
     if(argc!=3){
         return 1;
     }
-    funcs = ["wam","ddg" , "gl" , "jacobi"];
     for (i=0;i<4;i++){
-        if (argv[1] == funcs[i]){
+        if (strcmp(argv[1], funcs[i]) == 0){
             s = s+1;
         }
     }
     if (s == 0){
+        
         return 1;
     }
     return 0;
 }
 
 
-double** ll_to_arr(int num_of_dp, int size_of_vec, struct vector *dp_head){
+void ll_to_arr(int num_of_dp, int size_of_vec, struct vector *dp_head, double res[num_of_dp][size_of_vec]){ 
     struct vector *curr_vec;
     struct cord *curr_cord;
     int i,j;
-    double [num_of_dp][size_of_vec] res;
-
+    
     curr_vec = dp_head;
-    for (i=0;i<num_of_dp;i++){
+    for (i = 0; i < num_of_dp; i++){
         curr_cord = curr_vec->cords;
-        for (j=0;j<size_of_vec; j++){
+        for (j = 0; j < size_of_vec; j++){
             res[i][j] = curr_cord->value;
             curr_cord = curr_cord->next;
         }
         curr_vec = curr_vec->next;
     }
-    return res;
 }
