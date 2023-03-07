05361a58122b839d2dc21c460d5669314ee48c86
omri0702 <100294909+omri0702@users.noreply.github.com>
2023-03-07 21:04:01 +0200

    Update wam.c

--- Project/wam.c
+++ Project/wam.c
@@ -1,39 +1,41 @@
 #include <Python.h>
-#include "spkeamns.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
 # include <string.h>
+#include "spkmeans.h"
 
 
-int ** wam_c(int n, int[n][n] arr);
-double exp_euclidean_distance(int n double[n] x, double[n] y);
 
-int ** wam_c(int n, int [n][n] arr){
+double exp_euclidean_distance(int n, double x[n] , double y[n] );
+
+void wam_c(int n, int m, double arr[n][m] , double output_array[n][n]){
     int i,j;
-    double ret [n][n];
     for (i=0; i<n; i++){
         for(j=0;j<n;j++){
             if(i==j){
-                ret[i][j]=0;
+                output_array[i][j]=0;
             }
             else{
-                ret[i][j]= exp_euclidean_distance(arr[i],arr[j]);
+                output_array[i][j]= exp_euclidean_distance(m, arr[i],arr[j]);
             }
         }
     }
-    return ret;
 }
 
 
-double exp_euclidean_distance(int n double[n] x, double[n] y){
+double exp_euclidean_distance(int n, double x[n] , double y[n] ){
     double res;
     int i;
     double sum;
     sum=0;
+    res = 0;
     for (i=0;i<n;i++){
+
         sum = sum + pow((x[i]-y[i]),2);
     }
+    
+
     res = exp((-sum)/2);
     return res;
 }
