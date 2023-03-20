a5b5544a07dcbb306c816f53123a3afeae24a04f
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-03-20 16:50:40 +0200

    Update spkeamnsmodule.c

--- Project/spkeamnsmodule.c
+++ Project/spkeamnsmodule.c
@@ -1,17 +1,21 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include "spkmeans.h"
 
-void toPy(int n, double input[n+1][n], PyObject* output);
+
+
+PyObject* toPy(int n, double input[n+1][n], PyObject* output);
 void create_vec(int n, double curr[n], PyObject* row);
+void output_print(int num_of_dp,double  output_array[num_of_dp][num_of_dp]);
+void output_print_j(int num_of_dp,double output_array[num_of_dp+1][num_of_dp]);
 
-
-static PyObject* wrap_skp(PyObject *self, PyObject *args)
+static PyObject* wrap_spk(PyObject *self, PyObject *args)
 {
     int i,j;
     int num_of_dp, size_of_vec;
     PyObject *dp_arr_py;
     PyObject *item1, *item2;
-    Pyobject *Py_matrix;
+    PyObject *Py_matrix;
     double val;
 
     if(!PyArg_ParseTuple(args, "iiO", &num_of_dp, &size_of_vec,&dp_arr_py)) {
@@ -42,9 +46,8 @@ static PyObject* wrap_skp(PyObject *self, PyObject *args)
     gl_c(num_of_dp,ddg_array,wam_array,gl_array);
     jacobi_c(num_of_dp, gl_array, output_array);
 
-    toPy(n, output_array, Py_matrix);
-
-    return Py_matrix;
+    return toPy(num_of_dp, output_array, Py_matrix);
+     
 }  
 
 static void wrap_wam(PyObject *self, PyObject *args)
@@ -56,7 +59,7 @@ static void wrap_wam(PyObject *self, PyObject *args)
     double val;
 
     if(!PyArg_ParseTuple(args, "iiO", &num_of_dp, &size_of_vec,&dp_arr_py)) {
-        return NULL; 
+        return ; 
     }
 
     double cords[size_of_vec];
@@ -88,7 +91,7 @@ static void wrap_ddg(PyObject *self, PyObject *args)
     double val;
 
     if(!PyArg_ParseTuple(args, "iiO", &num_of_dp, &size_of_vec,&dp_arr_py)) {
-        return NULL; 
+        return;
     }
 
     double cords[size_of_vec];
@@ -122,7 +125,7 @@ static void wrap_gl(PyObject *self, PyObject *args)
     double val;
 
     if(!PyArg_ParseTuple(args, "iiO", &num_of_dp, &size_of_vec,&dp_arr_py)) {
-        return NULL; 
+        return ; 
     }
 
     double cords[size_of_vec];
@@ -157,7 +160,7 @@ static void wrap_jacobi(PyObject *self, PyObject *args){
     double val;
 
     if(!PyArg_ParseTuple(args, "iO", &n ,&dp_arr_py)) {
-        return NULL; 
+        return ; 
     }
     double row[n];
     double matrix[n][n];
@@ -176,12 +179,64 @@ static void wrap_jacobi(PyObject *self, PyObject *args){
     }
 
     jacobi_c(n, matrix, output_array);
-    output_print(num_of_dp, output_array);
+    output_print_j(n, output_array);
+}
+
+static PyObject* fit(PyObject *self, PyObject *args){
+    int k;
+    int iter;
+    double e;
+    PyObject *dp_arr_py;
+    int num_of_dp;
+    int size_of_vec;
+    PyObject *centroids_arr_py;
+    PyObject *item1, *item2;
+    double val;
+    int i, j;
+    
+
+    if(!PyArg_ParseTuple(args, "iidOOii", &k, &iter, &e, &dp_arr_py, &centroids_arr_py, &size_of_vec, &num_of_dp)) {
+        return NULL; 
+    }
+
+    double dp_arr[num_of_dp][size_of_vec];
+    double centroids_arr[k][size_of_vec];
+    double cords[size_of_vec];
+    
+    for(i=0; i<num_of_dp; i++){
+        item1 = PyList_GetItem(dp_arr_py,i);
+        for(j=0; j<size_of_vec; j++){
+            item2 = PyList_GetItem(item1, j);
+            val = PyFloat_AsDouble(item2);
+            cords[j] = val;
+        }
+        for(j=0; j<size_of_vec; j++){
+            dp_arr[i][j] = cords[j];
+        }
+
+    }
+
+    for(i=0; i<k; i++){
+        item1 = PyList_GetItem(centroids_arr_py,i);
+        for(j=0; j<size_of_vec;j++){
+            item2 = PyList_GetItem(item1, j);
+            val = PyFloat_AsDouble(item2);
+            cords[j] = val;
+        }
+        for(j=0; j<size_of_vec; j++) {
+            centroids_arr[i][j] = cords[j];
+        }
+    }
+
+
+
+    return kmeans_c( k, size_of_vec, num_of_dp, iter, e, dp_arr, centroids_arr); 
+
 }
 
 static PyMethodDef spkmeansMethods[] = {
-{"wrap_skp",
-(PyCFunction) wrap_skp,
+{"wrap_spk",
+(PyCFunction) wrap_spk,
 METH_VARARGS,
 PyDoc_STR("calcs the spkmeans algo from given data points")
 },
@@ -206,6 +261,12 @@ PyDoc_STR("calcs the Graph Laplacian for given data points")},
 METH_VARARGS,
 PyDoc_STR("calc the eigenvalue and eigenvectors of a given matrix")},
 
+{"fit",
+(PyCFunction) fit,
+METH_VARARGS,
+PyDoc_STR("runs the kmean algo from given data points, and centroids")
+},
+
 {NULL, NULL , 0, NULL}
 };
 
@@ -226,14 +287,15 @@ PyMODINIT_FUNC PyInit_myspkmeans(void){
     return m;
 }
 
-void toPy(int n, double input[n+1][n], PyObject* output){
+PyObject* toPy(int n, double input[n+1][n], PyObject* output){
     int i;
     PyObject* row;
     row = PyList_New(n);
-    for(i=0; i<n+1, i++){
+    for(i=0; i<n+1; i++){
         create_vec(n, input[i], row);
         PyList_SetItem(output, i, row);
     }
+    return output;
 }
 
 void create_vec(int n, double curr[n], PyObject* row){
@@ -245,4 +307,8 @@ void create_vec(int n, double curr[n], PyObject* row){
     }
 }
 
-
+void output_print(int num_of_dp,double output_array[num_of_dp][num_of_dp]){
+    
+}
+void output_print_j(int num_of_dp,double output_array[num_of_dp+1][num_of_dp]){
+}
