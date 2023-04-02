37e6f70d969278ae1d6983796f9bae9965e2773f
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-04-02 21:28:35 +0300

    Update setup.py

--- Project/setup.py
+++ Project/setup.py
@@ -1,6 +1,6 @@
 from setuptools import Extension, setup
 
-module = Extension("myspkmeans", sources=['spkmeansmodule.c'])
+module = Extension("myspkmeans", sources=['spkmeansmodule.c' ,'wam.c', 'ddg.c', 'gl.c', 'jacobi.c', 'kmeans.c', 'spkmeans.c'])
 setup(name='myspkmeans',
      version='1.0',
      description='Python wrapper for custom C extension',
