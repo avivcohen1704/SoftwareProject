443c2b53b2efaeb75e3480d2ec38115073d80224
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-04-02 21:29:37 +0300

    Update spkeamns.h

--- Project/spkeamns.h
+++ Project/spkeamns.h
@@ -1,5 +1,6 @@
 # ifndef SPKMEANS_H_
 # define SPKMEANS_H_
+#include <Python.h>
 
 struct cord
 {
@@ -12,6 +13,7 @@ struct vector
     struct cord *cords;
 };
 
+
 void wam_c(int n, int m, double arr[n][m] , double output_array[n][n]);
 
 void ddg_c(int n, double W[n][n], double D[n][n]);
@@ -20,6 +22,6 @@ void gl_c(int n, double D[n][n], double W[n][n], double output_array[n][n]);
 
 void jacobi_c(int n, double A[n][n], double output_array[n+1][n]);
 
-//PyObject* kmeans_c(int k, int size_of_vec , int num_of_dp,int iter, double e, double dp_arr[num_of_dp][size_of_vec], double centroids_arr[k][size_of_vec]);
+PyObject* kmeans_c(int k, int size_of_vec , int num_of_dp,int iter, double e, double dp_arr[num_of_dp][size_of_vec], double centroids_arr[k][size_of_vec]);
 
 # endif
