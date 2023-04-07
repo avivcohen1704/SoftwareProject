d433b07f0cbf5625c1bf4363468b4cf241359e69
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-04-07 16:33:03 +0300

    Update jacobi.c

--- Project/jacobi.c
+++ Project/jacobi.c
@@ -18,7 +18,8 @@ double sum_dot(int n, double *row, double *col); /* arrays of size n*/
 void calc_col(int j, int n, double **matrix, double *col ); /*matrix size n*n, col is array size n*/
 int check_convergeJ(int n, double **A , double **new_A); /* A and new_A is n*n */
 void add_eigenV(int n, double **A , double **V, double **res ); /* A and V is n*n res is n+1*n */
-void create_matrix(int n, int m, double **matrix);
+void create_matrix(int n, int m, double ***matrix_ptr);
+void print_mat(double **matrix , int n);
 
 
 
@@ -27,19 +28,24 @@ void jacobi_c(int n, double **A , double **res){
     double **V = NULL;
     double **P = NULL;
     double **new_A = NULL;
-    create_matrix(n,n,V); /*added this line*/
-    create_matrix(n,n,P); /*added this line*/
-    create_matrix(n,n,new_A); /*added this line*/
-    
+    create_matrix(n,n,&V); /*added this line*/
+    create_matrix(n,n,&P); /*added this line*/
+    create_matrix(n,n,&new_A); /*added this line*/
     
     ID(n,V);
-    
-
     for(iter=0;iter<max_iter;iter++){
         double theta, t, c, s;
         int pivot[2] ;
         
         max_off_diag(n,A, pivot);
+        if (fabs(A[pivot[0]][pivot[1]])<=eps){
+            for(i=0;i<n;i++){
+                for(j=0;j<n;j++){
+                    new_A[i][j] = A[i][j];
+            }
+        }
+            break;
+        }
         
         theta = (A[pivot[1]][pivot[1]]-A[pivot[0]][pivot[0]])/(2*A[pivot[0]][pivot[1]]);
         t = ((sign(theta))/(fabs(theta)+pow(pow(theta,2)+1,0.5)));
@@ -68,6 +74,12 @@ void jacobi_c(int n, double **A , double **res){
               
     }
     add_eigenV(n,new_A,V, res);
+
+    free_matrix(new_A,n);
+    free_matrix(V,n);
+    free_matrix(P,n);
+    
+    
 }
 
 
@@ -104,7 +116,8 @@ void rotation(int n,double s, double c, int pivot [2], double **A , double **new
     double **res = NULL;
     i = pivot[0];
     j = pivot[1];
-    create_matrix(n,n,res); /*added this line*/
+    create_matrix(n,n,&res); /*added this line*/
+    
 
     for (k=0;k<n;k++){
         for (l=0;l<n;l++){
@@ -135,6 +148,7 @@ void rotation(int n,double s, double c, int pivot [2], double **A , double **new
             new_A[l][k] = res[l][k];
         }
     }
+    free_matrix(res,n);
 }
 
 void ID(int n, double **res){
@@ -154,7 +168,7 @@ void dot(int n, double **a , double **b){
     int i,j;
     double *col = NULL;
     col = (double*)calloc(n, sizeof(double));
-    create_matrix(n,n,c); /*added this line*/
+    create_matrix(n,n,&c); /*added this line*/
 
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
@@ -170,6 +184,8 @@ void dot(int n, double **a , double **b){
             a[i][j] = c[i][j];
         }
     }
+    free_matrix(c,n);
+    free(col);
 }
 
 double sum_dot(int n, double *row, double *col){
@@ -228,10 +244,21 @@ void add_eigenV(int n, double **A , double **V, double **res){ /*res is n+1*n  *
 }
 
 
-void create_matrix(int n, int m, double **matrix){
+void create_matrix(int n, int m, double ***matrix_ptr){
     int i;
-    matrix = (double**)calloc(n, sizeof(double*));
+    double **matrix = (double**)calloc(n, sizeof(double*));
     for(i=0;i<n;i++){
         matrix[i] = (double*)calloc(m, sizeof(double));
     }
+    *matrix_ptr = matrix;
+}
+
+void print_mat(double **matrix , int n){
+    int i,j;
+    for(i=0; i<n+1; i++){
+        printf("\n");
+        for(j=0; j<n; j++){
+            printf("%f ", matrix[i][j]);}
+    }
+    printf("\n\n\n");
 }
