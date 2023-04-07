8bd16e88dc883eabd1843e10d1c4389e9e2c228d
Aviv Cohen <62901892+avivcohen1704@users.noreply.github.com>
2023-04-07 16:36:01 +0300

    Update spkeamns.h

--- Project/spkeamns.h
+++ Project/spkeamns.h
@@ -13,13 +13,16 @@ struct vector
 };
 
 
+
+
 void wam_c(int n, int m, double **arr , double **output_array);
 
 void ddg_c(int n, double **W, double **D);
 
-void gl_c(int n, double **D, double **W, double **L);
+void gl_c(int n, double **D, double **W, double **output_array);
 
-void jacobi_c(int n, double **A , double **res);
+void jacobi_c(int n, double **A, double **output_array);
 
+void free_matrix(double **matrix, int n);
 
 # endif
